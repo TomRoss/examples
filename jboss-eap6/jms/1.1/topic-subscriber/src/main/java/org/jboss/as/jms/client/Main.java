2d83097b14bfdb0e7c23e82ba64a81c965069405
Tom Ross <tom.ross@redhat.com>
2013-11-16 18:43:36 +0000

    Initial import.

---
+++ jboss-eap6/jms/1.1/topic-subscriber/src/main/java/org/jboss/as/jms/client/Main.java
@@ -0,0 +1,30 @@
+package org.jboss.as.jms.client;
+
+import java.util.logging.Logger;
+
+public class Main {
+	private static final Logger logger = Logger.getLogger(Main.class.getName());
+	
+	
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		
+		logger.info("<<< Starting Simple JMS Queue Producer >>>");
+		
+		Client client = new Client();
+		
+		client.runClient();
+		
+		logger.info("<<< shutting down Simple JMS Queue Producer >>>");
+		
+		System.exit(Globals.exitStatus);
+	}
+
+}
